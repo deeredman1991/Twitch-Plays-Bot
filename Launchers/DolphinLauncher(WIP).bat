05ecb496ec98b681d14f6e20f182c4bc1fe4df46
deeredman1991 <deeredman1991@gmail.com>
2016-08-21 08:20:36 -0400

    deleted everything

--- Launchers/DolphinLauncher(WIP).bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: TODO: Read paths from config file.
-set DolphinPath="J:\Games\Emus+Roms\GameCube\Emulators\Dolphin-x64\Dolphin"
-set gamePath="J:\Games\Emus+Roms\GameCube\Roms\Mario Party 7.iso"
-
-start "" %DolphinPath% -e %gamePath%
-
-exit 0
--- game/FightcadeLauncher.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: TODO: Read paths from config file.
-set FightCadePath="C:\Users\jamie_000\Desktop\FightCade\FightCade.exe"
-
-start "" %FightCadePath% 
-
-exit 0
