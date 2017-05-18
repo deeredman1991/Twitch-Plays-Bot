b7103379c1ba707f11aa997e06c80fa9c5976abd
TwitchPlaysDeveloper <kipor7aug@yahoo.co.uk>
2017-05-18 10:19:49 +0100

    Put ::--fullboot in the FightCade text and bug fix

--- game/FightcadeLauncher.bat
+++ game/FightcadeLauncher.bat
@@ -1,8 +1,8 @@
 @echo off
 
 :: TODO: Read paths from config file.
-set FightCadePath="C:\Users\jamie_000\Desktop\FightCade\FightCade.exe"
+set FightCadePath=""
 
-start "" %FightCadePath% 
+start "" %FightCadePath%::--fullboot
 
 exit 0
