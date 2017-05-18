4bb7b43587af265d5e3ff56605eec5d4749e1585
TwitchPlaysDeveloper <kipor7aug@yahoo.co.uk>
2017-05-18 10:17:07 +0100

    Fix bugs in text

--- Launchers/pcsx2Launcher.bat
+++ Launchers/pcsx2Launcher.bat
@@ -1,8 +1,8 @@
 @echo off
 
 :: TODO: Read paths from config file.
-set pcsx2Path="C:\Program Files (x86)\PCSX2 1.2.1\pcsx2-r5875.exe"
-set gamePath="J:\Games\Emus+Roms\PS2\Roms\Kingdom Hearts.iso"
+set pcsx2Path=""
+set gamePath=""
 
 start "" %pcsx2Path% %gamePath%
 ::--fullboot
